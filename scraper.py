e8bbfef1bbbf7b765864954c33c4f13461e75130
RonaldRaygun488 <jakelanc950@gmail.com>
2018-09-15 15:48:35 -0500

    Changed user agent string to look like a normal browser

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 pageToScrape = 'https://www.utdallas.edu/shop/parking/_code.php'
-page = urlopen(pageToScrape)
+page = urlopen(Request(pageToScrape, headers={'User-Agent': 'Mozilla'}))
 soup = BeautifulSoup(page, 'html.parser')
 h1 = soup.find('table', attrs={'summary':'Parking Structure 4'})
 h1Text = h1.tbody # this will contain each entry in the table for a single parking structure
@@ -10,4 +10,4 @@ h1Text = h1.tbody # this will contain each entry in the table for a single parki
 x = 0
 for child in h1Text:
     if hasattr(child,'attrs'):
-        print(child.td.next_sibling.next_sibling.next_sibling.next_sibling.contents)
+        print(list(child.td.next_sibling.next_sibling.next_sibling.next_sibling.children))
