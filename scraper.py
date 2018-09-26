9a118bbfd8b257f4d2dbb2d38e1e33e6b8e75374
RonaldRaygun488 <jakelanc950@gmail.com>
2018-09-26 11:34:34 -0500

    Finished function that will pull all the parking spots and compile them into a nice JSON array

--- scraper.py
+++ scraper.py
@@ -1,15 +1,33 @@
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
+import json
 pageToScrape = 'https://www.utdallas.edu/shop/parking/_code.php'
 page = urlopen(Request(pageToScrape, headers={'User-Agent': 'Mozilla'}))
 soup = BeautifulSoup(page, 'html.parser')
-h1 = soup.find('table', attrs={'summary':'Parking Structure 4'})
-h1Text = h1.tbody # this will contain each entry in the table for a single parking structure
-x = 0
-for child in h1Text:
-    if hasattr(child,'attrs'):
-        numSpace = str(child.td.next_sibling.next_sibling.next_sibling.next_sibling.contents[0])
-        if numSpace[0] == '<':
-            print(0)
-        else:
-            print(numSpace)
+parkingStructures = ['Parking Structure 1', 'Parking Structure 3', 'Parking Structure 4']
+finalList = []
+#List the amount of spots for every parking structure 1 - 4
+for structure in parkingStructures:
+    jsonObj = {'structure': structure}
+    print(structure)
+    parkingTable = soup.find('table', attrs={'summary':structure})
+    tableData = parkingTable.tbody # this will contain each entry in the table for a single parking structure
+    for child in tableData:
+        if hasattr(child,'attrs'):
+
+            color = str(child.td.next_sibling.next_sibling)[11:] #Get the permit type of the space and strip out the type from the html tag
+            color = color[:color.find('"')]
+            print(color)
+
+            numSpace = str(child.td.next_sibling.next_sibling.next_sibling.next_sibling.contents[0])
+            if numSpace[0] == '<':
+                print(0)
+                jsonObj[color] = 0
+            else:
+                print(numSpace)
+                jsonObj[color] = int(numSpace)
+    finalList.append(jsonObj)
+    print(json.dumps(jsonObj))
+    print('\n')
+
+print(json.dumps(finalList))
