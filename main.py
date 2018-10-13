818b2e9410fa313ffa73861c649403f9e69edd01
RonaldRaygun488 <jakelanc950@gmail.com>
2018-10-13 11:32:06 -0500

    Fixed bug with multiple levels having the same color space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
+from flask import Response
 import json
 pageToScrape = 'https://www.utdallas.edu/shop/parking/_code.php'
 page = urlopen(Request(pageToScrape, headers={'User-Agent': 'Mozilla'}))
@@ -11,25 +12,22 @@ def getParkingSpaces(request):
     #List the amount of spots for every parking structure 1 - 4
     for structure in parkingStructures:
         jsonObj = {'structure': structure}
-        print(structure)
         parkingTable = soup.find('table', attrs={'summary':structure})
-        tableData = parkingTable.tbody # this will contain each entry in the table for a single parking structure
+        tableData = parkingTable.tbody # the sub-table for a single parking structure
         for child in tableData:
             if hasattr(child,'attrs'):
 
                 color = str(child.td.next_sibling.next_sibling)[11:] #Get the permit type of the space and strip out the type from the html tag
+                level = str(child.td)[19:]
                 color = color[:color.find('"')]
-                print(color)
+                level = level[:level.find('<')]
+                color = color + '-' + level
 
                 numSpace = str(child.td.next_sibling.next_sibling.next_sibling.next_sibling.contents[0])
                 if numSpace[0] == '<':
-                    print(0)
                     jsonObj[color] = 0
                 else:
-                    print(numSpace)
                     jsonObj[color] = int(numSpace)
         finalList.append(jsonObj)
-        print(json.dumps(jsonObj))
-        print('\n')
-
-    return json.dumps(finalList)
+        resp = Response(json.dumps(finalList), mimetype='application/json')
+    return resp
